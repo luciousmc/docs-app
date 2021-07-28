dcb7d31640f9e501d349af9012e1c062e5f75680
Marlon Clay <luciousmc@icloud.com>
2021-07-28 15:55:58 -0700

    adds modal for creating new document

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,73 @@ import Header from '../components/Header';
 import Image from 'next/image';
 import { getSession, useSession } from 'next-auth/client';
 import Login from '../components/Login';
+import Modal from '@material-tailwind/react/Modal';
+import ModalBody from '@material-tailwind/react/ModalBody';
+import ModalFooter from '@material-tailwind/react/ModalFooter';
+import { useState } from 'react';
+import { db } from '../firebase';
+import firebase from 'firebase';
 
 export default function Home() {
   const [session] = useSession();
+  const [showModal, setShowModal] = useState(false);
+  const [input, setInput] = useState('');
   
   if(!session) return <Login />
+
+  const createDocument = () => {
+    if (!input) return;
+
+    db.collection('userDocs')
+      .doc(session.user.email)
+      .collection('docs')
+      .add({
+        fileName: input,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+      });
+
+    setInput('');
+    setShowModal(false);
+  };
+
+
+  const modal = (
+    <Modal 
+      size='sm'
+      active={showModal}
+      toggler={() => setShowModal(false)}
+    >
+      <ModalBody>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder='Enter name of document...'
+          onKeyDown={(e) => e.key === 'Enter' && createDocument()}
+          className='outline-none w-full'
+        />
+      </ModalBody>
+
+      <ModalFooter>
+        <Button
+          color='blue'
+          buttonType='link'
+          onClick={(e) => setShowModal(false)}
+          ripple='dark'
+        >
+          Cancel
+        </Button>
+
+        <Button
+          color='blue'
+          onClick={createDocument}
+          ripple='light'
+        >
+          Create
+        </Button>
+      </ModalFooter>
+    </Modal>
+  )
 
   return (
     <div>
@@ -18,6 +80,8 @@ export default function Home() {
       </Head>
 
       <Header />
+
+      {modal}
 
       <section className='bg-[#F8F9FA] pb-10 px-10'>
         <div className='max-w-3xl mx-auto'>
@@ -38,7 +102,7 @@ export default function Home() {
           </div>
 
           <div>
-            <div className='relative h-52 w-40 border-2 cursor-pointer hover:border-blue-700'>
+            <div onClick={() => setShowModal(true)} className='relative h-52 w-40 border-2 cursor-pointer hover:border-blue-700'>
               <Image
                 src='https://links.papareact.com/pju'
                 layout='fill'
