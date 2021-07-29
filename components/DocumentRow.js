cb1cb75aca9fb863cd40a17a14baf0163769e3d8
Marlon Clay <luciousmc@icloud.com>
2021-07-28 22:09:27 -0700

    add/style document row component

--- components/DocumentRow.js
+++ components/DocumentRow.js
@@ -3,8 +3,13 @@ import Icon from "@material-tailwind/react/Icon";
 import { useRouter } from 'next/dist/client/router';
 
 function DocumentRow({ id, fileName, date }) {
+  const router = useRouter();
+  
   return (
-    <div className='flex items-center p-4 rounded-lg hover:bg-gray-100 text-gray-700 text-sm cursor-pointer'>
+    <div
+      className='flex items-center p-4 rounded-lg hover:bg-gray-100 text-gray-700 text-sm cursor-pointer'
+      onClick={() => router.push(`/doc/${id}`)}
+    >
       <Icon name='article' size='3xl' color='blue' />
       <p className="flex-grow pl-5 w-10 pr-10 truncate">{fileName}</p>
       <p className="pr-5 text-sm">{date?.toDate().toLocaleDateString()}</p>
